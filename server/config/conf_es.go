ed7a5fc970eaa42da6f6b38005299125a08f6f8e
Crazy502 <llqweb@163.com>
2025-08-18 19:58:27 +0800

    docs(core): 添加JWT配置说明及使用流程

--- server/config/conf_es.go
+++ server/config/conf_es.go
@@ -7,3 +7,11 @@ type ES struct {
 	Password       string `json:"password" yaml:"password"`                 // Elasticsearch 登录密码
 	IsConsolePrint bool   `json:"is_console_print" yaml:"is_console_print"` // 是否在控制台打印 Elasticsearch 语句，true 表示打印，false 表示不打印
 }
+
+/*
+在博客项目中，Elasticsearch 可能用于：
+全文搜索 - 搜索文章内容、标题、标签等
+日志分析 - 分析用户访问日志、行为数据
+标签和分类统计 - 统计热门标签、分类等信息
+用户行为分析 - 分析用户阅读偏好等
+*/
