fbc6ef9c85018e76d401ce51ae913d77be0cb2b1
Crazy502 <llqweb@163.com>
2025-08-18 19:56:14 +0800

    docs(server): 添加验证码配置说明

--- server/config/conf_captcha.go
+++ server/config/conf_captcha.go
@@ -9,3 +9,8 @@ type Captcha struct {
 	MaxSkew  float64 `json:"max_skew" yaml:"max_skew"`   //单个数字的最大偏斜因子（绝对值
 	DotCount int     `json:"dot_count" yaml:"dot_count"` //背景圆点数量
 }
+
+//标签的作用是将结构体的字段与JSON或YAML数据进行绑定，并提供默认值。
+/*
+核心功能：需要一个验证码功能来防止机器人或恶意用户的垃圾邮件发送。
+*/
