8b2966eb68470c50cca748f6c5783da67f0086c3
Crazy502 <llqweb@163.com>
2025-09-20 21:11:21 +0800

    ``` chore(server): 移除生成随机密钥的注释代码
    从 main.go 文件中删除了已注释的 GenerateRandomKey 函数和相关测试代码，
    这些代码之前用于生成随机密钥但目前未被使用，清理后使代码更加简洁。
    ```

--- server/main.go
+++ server/main.go
@@ -23,3 +23,21 @@ func main() {
 
 	core.RunServer()
 }
+
+//func GenerateRandomKey(length int) (string, error) {
+//	key := make([]byte, length)
+//	if _, err := rand.Read(key); err != nil {
+//		return "", err
+//	}
+//	return base64.URLEncoding.EncodeToString(key), nil
+//}
+//
+//func main() {
+//	// 生成一个长度为 32 字节的随机密钥
+//	keyLength := 32
+//	randomKey, err := GenerateRandomKey(keyLength)
+//	if err != nil {
+//		panic(err)
+//	}
+//	fmt.Println("Randomly generated key:", randomKey)
+//}
